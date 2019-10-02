fd50aab867b7d7c6a53dfecbd7b04fe2799b39e4
JaredWilliams7485 <jaredwilliams7485@gmail.com>
2019-10-02 00:46:08 -0400

    Update SVM_Example.py

--- SVM_Example.py
+++ SVM_Example.py
@@ -1,31 +1,83 @@
-import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.svm import LinearSVC
-from sklearn.ensemble import GradientBoostingRegressor
+# From Python
+# It requires OpenCV installed for Python
+import sys
+import cv2
+import os
+from sys import platform
+import argparse
+import time
 
+# Import Openpose (Windows/Ubuntu/OSX)
+dir_path = os.path.dirname(os.path.realpath(__file__))
+try:
+    # Windows Import
+    if platform == "win32":
+        # Change these variables to point to the correct folder (Release/x64 etc.)
+        sys.path.append(dir_path + '/../../python/openpose/Release');
+        os.environ['PATH']  = os.environ['PATH'] + ';' + dir_path + '/../../x64/Release;' +  dir_path + '/../../bin;'
+        import pyopenpose as op
+    else:
+        # Change these variables to point to the correct folder (Release/x64 etc.)
+        sys.path.append('../../python');
+        # If you run `make install` (default path is `/usr/local/python` for Ubuntu), you can also access the OpenPose/python module from there. This will install OpenPose and the python library at your desired installation path. Ensure that this is in your python path in order to use it.
+        # sys.path.append('/usr/local/python')
+        from openpose import pyopenpose as op
+except ImportError as e:
+    print('Error: OpenPose library could not be found. Did you enable `BUILD_PYTHON` in CMake and have this Python script in the right folder?')
+    raise e
 
-#    ~other possible architectures~
-#rnd_clf = ExtraTreesClassifier(n_estimators=500, max_leaf_nodes=16, n_jobs=-1, random_state=42)
-#ada_clf = AdaBoostClassifier(
-#    DecisionTreeClassifier(max_depth=1), n_estimators=200,
-#    algorithm="SAMME.R", learning_rate=0.5, random_state=42)
-#gbrt = GradientBoostingRegressor(max_depth=2, n_estimators=3, learning_rate=1.0, random_state=42)
+# Flags
+parser = argparse.ArgumentParser()
+parser.add_argument("--image_dir", default="../../../examples/media/", help="Process a directory of images. Read all standard formats (jpg, png, bmp, etc.).")
+parser.add_argument("--no_display", default=False, help="Enable to disable the visual display.")
+args = parser.parse_known_args()
 
+# Custom Params (refer to include/openpose/flags.hpp for more parameters)
+params = dict()
+params["model_folder"] = "../../../models/"
 
-#    ~one line Random Forest~
-rnd_clf = RandomForestClassifier(n_estimators=500, max_leaf_nodes=16, n_jobs=-1, random_state=42)
+# Add others in path?
+for i in range(0, len(args[1])):
+    curr_item = args[1][i]
+    if i != len(args[1])-1: next_item = args[1][i+1]
+    else: next_item = "1"
+    if "--" in curr_item and "--" in next_item:
+        key = curr_item.replace('-','')
+        if key not in params:  params[key] = "1"
+    elif "--" in curr_item and "--" not in next_item:
+        key = curr_item.replace('-','')
+        if key not in params: params[key] = next_item
 
-#    ~import example training data~
-np_array = np.load("/Users/jaredwilliams/Documents/AI/GolfCart/out.npy")
-#print(np_array.shape)
+# Construct it from system arguments
+# op.init_argv(args[1])
+# oppython = op.OpenposePython()
 
-rand_input = np.random.rand(50000, 75)
-rand_output = np.random.randint(2, size=50000)
-#print(np_array.shape)
+try:
+    # Starting OpenPose
+    opWrapper = op.WrapperPython()
+    opWrapper.configure(params)
+    opWrapper.start()
 
-#fit expects a 2D array, so I need to flatten this from a nx25x3 into a nx75
-np_array = np.reshape(np_array, (-1, 75))
-rnd_clf.fit(rand_input, rand_output)
-print(rnd_clf.predict(np_array))
+    # Read frames on directory
+    imagePaths = op.get_images_on_directory(args[0].image_dir);
+    start = time.time()
+
+    # Process and display images
+    for imagePath in imagePaths:
+        datum = op.Datum()
+        imageToProcess = cv2.imread(imagePath)
+        datum.cvInputData = imageToProcess
+        opWrapper.emplaceAndPop([datum])
+
+        print("Body keypoints: \n" + str(datum.poseKeypoints))
+
+        if not args[0].no_display:
+            cv2.imshow("OpenPose 1.5.1 - Tutorial Python API", datum.cvOutputData)
+            key = cv2.waitKey(15)
+            if key == 27: break
+
+    end = time.time()
+    print("OpenPose demo successfully finished. Total time: " + str(end - start) + " seconds")
+except Exception as e:
+    # print(e)
+    sys.exit(-1)
